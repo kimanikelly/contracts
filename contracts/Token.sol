618da71e2606081a41cfb259c6f9840c9eb4d5e3
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-03-17 23:37:42 -0400

    Refactored the constructor to an initializer for upgradeability

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,22 +5,22 @@ import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Ini
 import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is Initializable,ERC20Upgradeable,Ownable {
+contract Token is Initializable,ERC20Upgradeable {
 
 
-    function initialize(string memory name, string memory symbol) public  {
-        
+    function initialize(string memory name, string memory symbol) public initializer  {
+        __ERC20_init(name,symbol);
     }
 
-   function fund(uint256 amount) public onlyOwner {
-        require(amount > 0, "Token: Insufficient fund amount");
-        _mint(address(this), amount);
-    }
+//    function fund(uint256 amount) public onlyOwner {
+//         require(amount > 0, "Token: Insufficient fund amount");
+//         _mint(address(this), amount);
+//     }
 
-    function fundAccount(uint256 amount) public {
-        require(amount <= 100, "Token: Mint limit exceeded");
-        ERC20(address(this)).transfer(msg.sender, 100);
-    }
+//     function fundAccount(uint256 amount) public {
+//         require(amount <= 100, "Token: Mint limit exceeded");
+//         ERC20(address(this)).transfer(msg.sender, 100);
+//     }
 }
 
 
