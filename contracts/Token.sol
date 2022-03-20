408d544b8dad2f6ae8a0d4fb909ea4484682a408
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-03-19 23:24:22 -0400

    Remove the hardhat/console.sol from the Token contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.12;
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "hardhat/console.sol";
 
 contract Token is Initializable, OwnableUpgradeable, ERC20Upgradeable {
     /**
