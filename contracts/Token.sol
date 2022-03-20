ee7bf842137075bbd29da89ccce3a3f1a715d001
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-03-19 21:59:43 -0400

    Comments for the FundAmountSet event

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,29 +6,38 @@ import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "hardhat/console.sol";
 
-contract Token is Initializable,OwnableUpgradeable,ERC20Upgradeable{
-    
+contract Token is Initializable, OwnableUpgradeable, ERC20Upgradeable {
+
+    /**
+     * @dev Emitted when the owner invokes `setFundAmount` to change the 
+     * `fundAmount` value.
+     * @param previousAmount The old `fundAmount` to be replaced.
+     * @param newAmount The new `fundAmount` set to replace the `previousAmount`.
+     */
     event FundAmountSet(uint256 previousAmount, uint256 newAmount);
 
+    ///
     uint256 public fundAmount;
 
     uint256 internal amountCheck;
 
-    function initialize(string memory name, string memory symbol) public initializer  {
-        __ERC20_init(name,symbol);
+    function initialize(string memory name, string memory symbol)
+        public
+        initializer
+    {
+        __ERC20_init(name, symbol);
 
         __Ownable_init();
 
         amountCheck = 0;
     }
 
-   function mint(uint256 amount) public onlyOwner {
+    function mint(uint256 amount) public onlyOwner {
         require(amount > amountCheck, "Token: Amount cannot be 0");
         _mint(address(this), amount);
     }
 
-    function setFundAmount(uint256 amount) public onlyOwner  {
-
+    function setFundAmount(uint256 amount) public onlyOwner {
         // The value of the current fundAmount
         uint256 previousAmount = fundAmount;
 
@@ -40,15 +49,10 @@ contract Token is Initializable,OwnableUpgradeable,ERC20Upgradeable{
 
         // Emits the SetFundAmount event with the previousAmount and amount arguments
         emit FundAmountSet(previousAmount, amount);
-       
     }
 
     function fundAccount() public {
-
         // Transfers the fundAmount to the msg.sender
         ERC20Upgradeable(address(this)).transfer(msg.sender, fundAmount);
-
     }
 }
-
-
