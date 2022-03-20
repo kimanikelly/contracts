22c963b0ede851d5a6f148f6093eec70aed240de
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-03-20 00:23:26 -0400

    Able to deploy the Token contract as an upgradeable contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -13,13 +13,15 @@ async function main() {
   // manually to make sure everything is compiled
   // await hre.run('compile');
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const signers = await ethers.getSigners();
 
-  await greeter.deployed();
+  const Token = await ethers.getContractFactory("Token", signers[0]);
 
-  console.log("Greeter deployed to:", greeter.address);
+  const token = await upgrades.deployProxy(Token, ["Test Token", "TT"]);
+
+  await token.deployed();
+
+  console.log(`Token deployed to ${token.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
