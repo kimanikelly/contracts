ef4ebe84a0dee6a370e15caf06c3c0e0ced92b95
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-06-28 15:40:28 -0400

    Building ILender Added the Vault struct containing the properties of the borrower

--- contracts/interfaces/ILender.sol
+++ contracts/interfaces/ILender.sol
@@ -1,2 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
+
+/// Loan details of a borrower
+struct Vault {
+    address borrower; /// Address of the borrower
+    address nftCollateralAddress; /// Address of the collateral ERC-721 contract
+    uint256 nftCollateralTokenId; /// TokenId of the collateral ERC-721 token
+    uint256 principalAmount; /// ERC-20 loan amount
+    uint256 dateOfLoan; /// Date the loan was taken out
+    uint256 loanMaturityDate; /// Date the loan matures
+}
