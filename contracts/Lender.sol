5e1911d56e1f960d9762d383d0fcff368bcee412
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-06-29 14:40:49 -0400

    Building Lender Imported Initialable Inherited Initializable Implemented the initialize function Added the  __Ownable_init inside the body of the initialize function

--- contracts/Lender.sol
+++ contracts/Lender.sol
@@ -4,10 +4,21 @@ pragma solidity 0.8.9;
 /// Imports the ILender interface
 import {ILender} from "./interfaces/ILender.sol";
 
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
 /// Imports the OwnableUpgradeable contract
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 /// Imports the ERC721HolderUpgradeable contract and allows Lender to receive ERC-721's
 import {ERC721HolderUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/utils/ERC721HolderUpgradeable.sol";
 
-contract Lender is OwnableUpgradeable, ERC721HolderUpgradeable {}
+contract Lender is Initializable, OwnableUpgradeable, ERC721HolderUpgradeable {
+    /**
+     * @dev Deploys Lender.sol as an upgradeable smart contract by refactoring the
+     * `constructor` with the `initialize` function
+     */
+    function initialize() public initializer {
+        /// Initializes OwnableUpgradeable.sol and assigns the msg.sender as the owner
+        __Ownable_init();
+    }
+}
