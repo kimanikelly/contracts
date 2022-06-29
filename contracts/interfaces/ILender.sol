57e61b2dc91000f3b722cb7dad4f0a9861b7395d
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-06-28 20:47:32 -0400

    Building Ilender Declared the LoanBorrowed event

--- contracts/interfaces/ILender.sol
+++ contracts/interfaces/ILender.sol
@@ -1,12 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-/// Loan details of a borrower
-struct Vault {
-    address borrower; /// Address of the borrower
-    address nftCollateralAddress; /// Address of the collateral ERC-721 contract
-    uint256 nftCollateralTokenId; /// TokenId of the collateral ERC-721 token
-    uint256 principalAmount; /// ERC-20 loan amount
-    uint256 dateOfLoan; /// Date the loan was taken out
-    uint256 loanMaturityDate; /// Date the loan matures
+interface ILender {
+    /// Loan details of a borrower
+    struct Vault {
+        address borrower; /// Address of the borrower
+        address nftCollateralAddress; /// Address of the collateral ERC-721 contract
+        uint256 nftCollateralTokenId; /// TokenId of the collateral ERC-721 token
+        uint256 principalAmount; /// ERC-20 loan amount
+        uint256 dateOfLoan; /// Date the loan was taken out
+        uint256 loanMaturityDate; /// Date the loan matures
+    }
+
+    /// Emits the loan details after the borrower invokes the borrow() function
+    event LoanBorrowed(
+        address borrower, /// Address of the borrower
+        address nftCollateralAddress, /// Address of the collateral ERC-721 contract
+        uint256 nftCollateralTokenId, /// TokenId of the collateral ERC-721 token
+        uint256 principalAmount, /// ERC-20 loan amount
+        uint256 dateOfLoan, /// Date the loan was taken out
+        uint256 loanMaturityDate /// Date the loan matures
+    );
 }
