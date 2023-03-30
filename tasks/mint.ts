b03961099594e80f3c7ecdc2e72bf34f56b12a34
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2023-03-30 07:45:00 -0400

    Comment out the mint task for now

--- tasks/mint.ts
+++ tasks/mint.ts
@@ -1,31 +1,32 @@
-import { task } from "hardhat/config";
-import { Token__factory } from "../typechain";
+// import { task } from "hardhat/config";
 
-task(
-  "mint",
-  "Mints a number of Test Tokens to the Token contract. Can only be called by the owner."
-)
-  .addParam("address", "The address of Token contract")
-  .addParam(
-    "amount",
-    "The amount of Test Tokens the Token contract should mint"
-  )
-  .setAction(async (taskArgs, { ethers }) => {
-    const signer = await ethers.getSigners();
+// import { Token__factory } from "../typechain";
 
-    const token = Token__factory.connect(taskArgs.address, signer[0]);
+// task(
+//   "mint",
+//   "Mints a number of Test Tokens to the Token contract. Can only be called by the owner."
+// )
+//   .addParam("address", "The address of Token contract")
+//   .addParam(
+//     "amount",
+//     "The amount of Test Tokens the Token contract should mint"
+//   )
+//   .setAction(async (taskArgs, { ethers }) => {
+//     const signer = await ethers.getSigners();
 
-    const tx = await token.mint(taskArgs.amount);
+//     const token = Token__factory.connect(taskArgs.address, signer[0]);
 
-    let obj = {
-      transactionHash: `https://rinkeby.etherscan.io/tx/${tx.hash}`,
-      mintAmount: taskArgs.amount,
-    };
+//     const tx = await token.mint(taskArgs.amount);
 
-    if (ethers.provider.network.chainId == 4) {
-      console.log(obj);
-    } else if (ethers.provider.network.chainId == 31337) {
-      obj["transactionHash"] = `Localhost Hash: ${tx.hash}`;
-      console.log(obj);
-    }
-  });
+//     let obj = {
+//       transactionHash: `https://rinkeby.etherscan.io/tx/${tx.hash}`,
+//       mintAmount: taskArgs.amount,
+//     };
+
+//     if (ethers.provider.network.chainId == 4) {
+//       console.log(obj);
+//     } else if (ethers.provider.network.chainId == 31337) {
+//       obj["transactionHash"] = `Localhost Hash: ${tx.hash}`;
+//       console.log(obj);
+//     }
+//   });
